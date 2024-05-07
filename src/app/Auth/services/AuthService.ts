10950395060f4112defc998e07469724b9c23036
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-05-07 19:34:06 -0300

    fix: config auth

--- src/app/Auth/services/AuthService.ts
+++ src/app/Auth/services/AuthService.ts
@@ -1,14 +1,33 @@
+import jwt from "jsonwebtoken";
+import AuthError from "@app/Auth/exceptions/AuthError";
+import config from "@/config"
+
 export default class AuthService {
-    async signIn(email: string, password: string): Promise<void> {
+    async signIn(email: string, password: string): Promise<{ user: object; token: string }> {
         const user = {
             id: '123',
             email: 'example@email.com',
             password: '123abc',
-            fullname: 'Admin',
+            fullName: 'Admin',
         }
 
         if(email !== user.email || password !== user.password) {
-            throw new Error ('Invalid credentials!')
+            throw new AuthError ('Invalid credentials!')
+        }
+
+        const { id, fullName } = user 
+
+        const token = jwt.sign({ id }, config.auth.secret, {
+            expiresIn: config.auth.expiresIn,
+        })
+
+        return {
+            user: {
+                id,
+                fullName,
+                email,
+            },
+            token,
         }
     }
 }
